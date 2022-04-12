f9eea8193e37c47a8176f1d9875b58192344308e
hobit22 <hobit22@naver.com>
2022-04-12 15:53:57 +0900

    Initial commit

---
+++ src/test/java/com/practice/s3test/S3testApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.s3test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3testApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
